163c34902dfd1c78d3ee1c701964753d7907bd32
Jacob Deinum <deinum@ualberta.ca>
2022-10-12 12:44:11 -0600

    update analysis

--- code/clean.sh
+++ code/clean.sh
@@ -4,7 +4,7 @@
 tcpdump -r $1 ether host e8:50:8b:43:b1:20 -tt --time-stamp-precision=nano |
 
 # extract only the time and the channel it was sent on
-awk '{if ($4 ~ /bad-fcs/) {print $1" "$7} else {print $1" "$6}}' |
+awk '{if ($4 ~ /bad-fcs/) {print $1" "$7" " $12} else if ($4 ~ /short/) {print $1" "$9" "$14} else {print $1" "$6" "$11}}' |
 
 awk ' {gsub(2412,1); gsub(2437,6); gsub(2462,11); print $0 }' |
 
