f7790760014fe1a050c6815c33db6f7e40c00cdd
deinum <deinum@ualberta.ca>
2022-11-16 09:12:41 -0700

    merging

---
+++ code/filter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tshark -r $1 -T fields \
+  -e frame.time_epoch \
+  -e wlan_radio.frequency \
+  -e wlan_radio.signal_dbm \
+  -e wlan.ta \
+  -e wlan.ra \
+  -e wlan.seq \
+  -e wlan.ssid \
+  -e wlan.bssid > $2
