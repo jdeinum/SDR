23999d06cd2873ff3ec47ed15761664886c1b35a
Jacob Deinum <deinum@ualberta.ca>
2022-11-16 09:48:26 -0700

    adding new script for more data collection

---
+++ code/capture.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+# first let's get the date and the hour
+hour=$(date +%H)
+now=$(date +%d_%m_%y)
+log=/tmp/mylog.out
+CAPTURE_TIME=30
+
+
+
+# first capture of the day, prep for the rest of the day
+if [[ $hour == "00" ]]; then
+
+  echo "$(date) Creating directories and turning on interfaces" > $log
+  mkdir -p "/home/deinum/sdr/data/pcap/hourly/$now" 
+  mkdir -p "/home/deinum/sdr/data/text/hourly/$now"
+
+  ip link set wlx00127b216d36 down
+  ip link set wlx00127b216d1e down
+  ip link set wlx00127b216d41 down
+
+  iw wlx00127b216d36 set monitor fcsfail
+  iw wlx00127b216d1e set monitor fcsfail
+  iw wlx00127b216d41 set monitor fcsfail
+
+  ip link set wlx00127b216d36 up
+  ip link set wlx00127b216d1e up
+  ip link set wlx00127b216d41 up
+
+  iw wlx00127b216d36 set channel 1
+  iw wlx00127b216d1e set channel 6
+  iw wlx00127b216d41 set channel 11
+fi
+
+echo "$(date) Starting scan" > $log
+timeout "$CAPTURE_TIME"m tcpdump -i wlx00127b216d36 type mgt subtype probe-req -w "/home/deinum/sdr/data/channel-1.pcap" &
+timeout "$CAPTURE_TIME"m tcpdump -i wlx00127b216d1e type mgt subtype probe-req -w "/home/deinum/sdr/data/channel-6.pcap" &
+timeout "$CAPTURE_TIME"m tcpdump -i wlx00127b216d41 type mgt subtype probe-req -w "/home/deinum/sdr/data/channel-11.pcap" &
+sleep "$CAPTURE_TIME"m
+
+echo "$(date) Scan Finished" > $log
+
+# combine pcap files into a single file and remove the uneccessary stuff
+echo "$(date) Merging PCAP files" > $log
+mergecap -w $OFILE /home/deinum/sdr/data/*.pcap
+rm /home/deinum/sdr/*.pcap
+
+echo "$(date) Converting PCAP to TEXT" > $log
+/home/deinum/sdr/code/filter.sh "/home/deinum/sdr/data/pcap/hourly/$now/sample$hour.pcap" "/home/deinum/sdr/data/text/hourly/$now/sample$hour.txt" 
+
+# last scan of the day
+if [[ $hour == "23" ]]; then
+  echo "$(date) Turning off interfaces" > $log
+  ip link set wlx00127b216d36 down
+  ip link set wlx00127b216d1e down
+  ip link set wlx00127b216d41 down
+end
+
+
+
+
+
+
+
