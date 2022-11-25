f884a7286df3216167661d5c75c1e5ab21f53db1
Jacob Deinum <deinum@ualberta.ca>
2022-11-25 07:43:27 -0700

    Fri Nov 25 07:43:27 AM MST 2022 | Auto Merged

--- code/capture.sh
+++ code/capture.sh
@@ -58,6 +58,7 @@ echo "$(date) Adding previous channel using AWK" >> $log
 cat "/home/deinum/sdr/data/text/hourly/$now/sample$hour.txt" | 
 mawk 'NR == 1 {prev_channel=$2; start_time=$1} NR > 1 {printf "%f %f %d %d %d %d %s\n" ,$1, $1 - start_time, $2, prev_channel, $3, $6, $4; prev_channel=$2}' > /tmp/clean.txt
 mv /tmp/clean.txt "/home/deinum/sdr/data/text/hourly/$now/sample$hour.txt" && rm /tmp/clean.txt
+chown -R deinum "/home/deinum/sdr/data/text/hourly/$now/sample$hour.txt"
 
 # last scan of the day
 if [[ $hour == "23" ]]; then
