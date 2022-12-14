44355ab294077a889a67058939246efdfd080c3b
Jacob Deinum <deinum@ualberta.ca>
2022-12-14 10:25:03 -0700

    Wed Dec 14 10:25:02 AM MST 2022 | Auto Merged

--- code/clean.sh
+++ code/clean.sh
@@ -5,7 +5,8 @@ awk -F"\t" '{for(N=1; N<=6; N++) if($N=="") break; else if(N==6) print }' |
 # sort by the address and the time
 sort -k4,4 -k1,1n |
 
-# now add the previous channel
+# now add the previous channel and the time relative to the first packet seen
+# from an address
 mawk -f /home/deinum/Work/sdr/code/clean.awk
 
 
